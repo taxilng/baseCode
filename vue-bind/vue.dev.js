6f567fa2c4bab62400130593377f2e2083b2d3e3
telanks <769967440@qq.com>
2018-05-08 21:40:03 +0800

    rest

--- vue-bind/vue.dev.js
+++ vue-bind/vue.dev.js
@@ -1,1 +1,11 @@
 'use strict';
+function myVue(options) {
+    this._init(options);
+  }
+  myVue.prototype._init = function (options) {
+      this.$options = options;  // options 为上面使用时传入的结构体，包括el,data,methods
+      this.$el = document.querySelector(options.el); // el是 #app, this.$el是id为app的Element元素
+      this.$data = options.data; // this.$data = {number: 0}
+      this.$methods = options.methods;  // this.$methods = {increment: function(){}}
+    }
+  
