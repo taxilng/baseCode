512573e93980daa84671d840c59bc51487066b7b
taxilng <769967440@qq.com>
2018-11-20 15:50:58 +0800

    flat 增强版，支持传参啦

--- polyfill/flat.js
+++ polyfill/flat.js
@@ -1,10 +1,13 @@
 if (!Array.prototype.flat) {
-    Array.prototype.flat = function() {
+    Array.prototype.flat = function (num = 1) {
+        if (!Number(num)) {
+            return this;
+        }
         var arr = []
         this.forEach((item) => {
-            if(Array.isArray(item)){
-                arr = arr.concat(item.flat())
-            }else{
+            if (Array.isArray(item)) {
+                arr = arr.concat(item.flat(--num))
+            } else {
                 arr.push(item)
             }
         })
@@ -12,12 +15,21 @@ if (!Array.prototype.flat) {
     }
 }
 
-if (!Array.prototype.flat) {
-    Array.prototype.flat = function () {
-        return this.toString().split(',').map(item => +item || item)
-    }
-}
+// if (!Array.prototype.flat) {
+//     Array.prototype.flat = function (num) {
+//         console.log(num);
+//         return this.toString().split(',').map(item => +item || item)
+//     }
+// }
 
 // test
-const arr = [1, [2, [3, 'a']]]
-console.log(arr.flat());
+const arr = [1, [2, [3, 'a', [4]]]]
+
+console.log(arr.flat('dsdsadf'));
+console.log(arr.flat(-32));
+console.log(arr.flat(0));
+console.log(arr.flat('1'));
+console.log(arr.flat('2'));
+console.log(arr.flat(3));
+console.log(arr.flat(Infinity));
+console.log(arr.flat('Infinity'));
