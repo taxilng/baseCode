815fdd3065878d18594e9f343ac9793947f4c23e
taxilng <769967440@qq.com>
2018-11-22 14:13:52 +0800

    移动add函数

--- ES6/module/module6/main.js
+++ ES6/module/module6/main.js
@@ -1,5 +1,8 @@
 // import {foo} from './middleware.js';
 // console.log(foo);
 
-import add from './middleware.js';
-console.log(add);
+// import add from './middleware.js';
+// console.log(add);
+
+import { es6 } from './middleware.js';
+console.log(es6(2,3));
--- ES6/module/module6/middleware.js
+++ ES6/module/module6/middleware.js
@@ -1,4 +1,5 @@
 // export {foo} from './profile.js';
 // export * from './profile.js';
 // export { default }from './profile.js';
-export { foo as default }from './profile.js';
+// export { foo as default }from './profile.js';
+export { default as es6 }from './profile.js';
