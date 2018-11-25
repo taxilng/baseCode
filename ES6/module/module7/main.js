7b9e11b0c03eab1c113e7b07dbb41b42f8f5ea54
telanks <769967440@qq.com>
2018-11-26 01:47:46 +0800

    module 继承

---
+++ ES6/module/module7/main.js
@@ -0,0 +1,4 @@
+import * as math from './middleware.js';
+import exp from './middleware.js';
+console.log(exp(math.e));
+console.log(math);
---
+++ ES6/module/module7/middleware.js
@@ -0,0 +1,5 @@
+export * from './profile.js';
+export var e = 2.71828182846;
+export default function (x) {
+    return Math.exp(x);
+}
