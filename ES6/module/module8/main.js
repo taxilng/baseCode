b923a9ee8600055dd0492cf411e42e02aab44912
taxilng <769967440@qq.com>
2018-11-26 09:24:32 +0800

    test

---
+++ ES6/module/module8/main.js
@@ -0,0 +1,8 @@
+// import {foo} from './middleware.js';
+// console.log(foo);
+
+// import add from './middleware.js';
+// console.log(add);
+
+import { add } from './middleware.js';
+console.log(add(2,3));
---
+++ ES6/module/module8/profile.js
@@ -0,0 +1,9 @@
+export var foo = 'bar';
+// function aa(){
+//     export default 2;
+// }
+function add(x, y) {
+    return x * y;
+}
+// export { add as default };
+export default add;
