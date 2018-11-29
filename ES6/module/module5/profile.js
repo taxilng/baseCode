99a38e16c8f2e448cea80e2eee93f855b677d08f
taxilng <769967440@qq.com>
2018-11-29 09:30:39 +0800

    sort

--- ES6/module/module5/profile.js
+++ ES6/module/module5/profile.js
@@ -3,7 +3,7 @@
 //     export default 2;
 // }
 function add(x, y) {
-    return x * y;
+  return x * y
 }
-// export { add as default };
-export default 1;
+export { add as default }
+// export default 1;
